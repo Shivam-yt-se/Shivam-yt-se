4f7ba629a317dda719bf78bfa8f17e12c61d21ce
Shivam Gupta <200709shivam@gmail.com>
2022-02-22 20:08:30 +0530

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import bcg_img from "../img/cheerful-attractive-young-woman-with-black-hair-walking.jpg";
+import "./Home.css"
+
+const Home = () => {
+  return (
+      <div>
+          <img className="home_img" src={bcg_img} alt="college-girl"/>
+    </div>
+  )
+}
+
+export default Home
